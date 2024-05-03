86ca7586ab01f1d0c70877bcbe0ee8f4d8cb8363
nehimyamesfin <166433975+nehimyamesfin@users.noreply.github.com>
2024-05-03 14:14:56 +0300

    Removed Files

---
+++ Ex.py
@@ -0,0 +1,13 @@
+
+'''
+gtst='day7'
+print(f'Hello today is our {gtst} course')
+
+# numbers=[0,1,2,3,4,5]
+# print('The first even no is', numbers(0))
+
+
+fruit={'apple':10,'bannana':15,'pineapple':20}
+print(fruit(0))
+'''
+
